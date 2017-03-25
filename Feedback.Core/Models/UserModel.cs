732c37349b69f9d7094fcc433c024e82dc829b73
,Yunus Emre ÇAKMAZ <ynsemre158@gmail.com>
2017-03-25 14:39:50 +0300

    Initial Commit

---
+++ Feedback.Core/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Feedback.Core.Models
+{
+    [Table("UserSet")]
+    public class UserModel : BaseModel
+    {
+        public string Name { get; set; }
+    }
+}
